bf11c7a0697fe0f7462864bb7f8058f3a970d535
SureshRyali <suresh.march10@gmail.com>
2023-06-26 23:06:59 +0530

    Added ping controller

--- backend/src/main/java/com/amigoscode/PingPongController.java
+++ backend/src/main/java/com/amigoscode/PingPongController.java
@@ -1,7 +1,9 @@
 package com.amigoscode;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
 public class PingPongController {
 	  private static int COUNTER = 0;
 
